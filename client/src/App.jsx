4daee62a8f6a8048ae84d9e4d9ef266bf7af82b1
JoacogaGit <Joaquin.gallea@gmail.com>
2025-10-20 01:19:59 -0300

    ajuste

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,51 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MarcasPage from './components/MarcasPage'
 import Home from './views/Home'
-import Navigation from './views/Navigation'
-import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 import Contact from './views/Contact'
 import NavBar from './components/NavBar'
-import CardList from './components/CardList'
 import ColeccionableDestacado from './components/ColeccionableDestacado'
-
-function App() {
-  const navigate = useNavigate()
-  const location = useLocation()
-
-  const handleClick = ()=>{
-    navigate('/home')
-  }
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 export default function App() {
   return (
     <>
-    <NavBar/>
-    {/*<Navigation/>*/}
-    <Routes>
-      <Route path="/home" element={<Home/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/marcas" element={<MarcasPage/>}/>
-    </Routes>
-    {/*<CardList/>*/}
-    <ColeccionableDestacado colId={1}/>
-    </>
-  )
-}
-
-      {/* Rutas */}
+      <NavBar />
       <Routes>
-        {/* redirigir raíz a /home */}
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/home" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-
-        {/* 404 simple */}
-        <Route path="*" element={<div className="p-8">404 — Not found</div>} />
+        <Route path="/marcas" element={<MarcasPage />} />
+        <Route path="*" element={<div className="p-8">404 - Not found</div>} />
       </Routes>
-    </div>
-  );
+      {/*<CardList/>*/}
+      <ColeccionableDestacado colId={1} />
+    </>
+  )
 }
